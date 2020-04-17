38c2a873a519e738ab927a54ac378a63c53c89d9
helloyako <helloyako@gmail.com>
2020-04-17 11:57:14 +0900

    Chagne Wind.deg to optional

--- Weather/Model/Wind.swift
+++ Weather/Model/Wind.swift
@@ -10,13 +10,16 @@ import Foundation
 
 struct Wind: Codable {
     let speed: Double
-    let deg: Int
+    let deg: Int?
     
     var description: String {
         return "\(degreeToCompass()) \(Int(speed)) m/s"
     }
     
     private func degreeToCompass() -> String {
+        guard let deg = deg else {
+            return ""
+        }
         let val = Int((Double(deg) / 22.5) + 0.5)
         let arr = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
         return arr[(val % 16)]
